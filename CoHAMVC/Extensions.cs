f8007f84809f25e0e644b51bb17e7d566fd35643
Ian Baker <Ian.Baker@contractor.prometric.com>
2020-07-03 02:45:36 -0400

    Moved IQueryable.ToDictionary() to the MVC shared project.

---
+++ CoHAMVC/Extensions.cs
@@ -0,0 +1,16 @@
+﻿namespace MiraThree
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.Primitives;
+
+    public static class Extensions
+    {
+        public static Dictionary<string, string> ToDictionary(this IQueryCollection queryCollection)
+        {
+            return queryCollection.ToDictionary<KeyValuePair<string, StringValues>, string, string>
+                (kvp => kvp.Key, kvp => kvp.Value);
+        }
+    }
+}
